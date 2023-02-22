884d7a686df65a067bd30654f27589c01dbd1cc0
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-02-22 14:01:19 +0600

    const and let difference

--- variable.js
+++ variable.js
@@ -4,3 +4,23 @@ a = 'Rakib';
 console.log(a);
 
 //here a gives two outputs 
+let c = 3;
+c= 'Rakib';
+console.log(c);
+
+{
+    let a = 1000;
+    console.log(a);
+}
+
+//let can be used in blocks. but we can't declare it twice in a single block . but we can change it's value
+
+const rakib = 'react';
+// rakib = 5;  this is error because we can't change const values
+
+{
+    const rakib = 'lll'
+    console.log(rakib);
+}
+
+console.log(rakib);
