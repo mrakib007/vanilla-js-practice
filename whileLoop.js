2314aff1da2bd74d3abab5b5aebba0150cc61706
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-02-24 11:31:25 +0600

    do while loop

--- whileLoop.js
+++ whileLoop.js
@@ -2,7 +2,15 @@ let a = 10;
 // let a = prompt("Enter a number");
 // a = Number.parseInt(a);
 let i = 0;
+let n = 100;
 while (i<n){
     console.log(i);
     i++;
 }
+
+do{
+    console.log(i);
+    i++;
+}while(i<n);
+
+//Do while loop will be executed at least one time regardless of the condition it is given.
