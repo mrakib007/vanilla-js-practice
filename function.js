ba3d0ae3875993f0bf1226788a81ada210684013
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-02-25 10:37:28 +0600

    simple function

---
+++ function.js
@@ -0,0 +1,15 @@
+let a = 1;
+let b = 2;
+let c = 3;
+
+function onePlusAvg(x,y){
+    console.log('done');
+    return 1+(x+y/2);
+}
+const sum = (p,q) =>{
+    return p+q;
+}
+
+console.log("avg = ",onePlusAvg(a,b)); 
+console.log("avg = ",onePlusAvg(b,c)); 
+console.log('done');
