dccc06895fa1335aba83d67ac7e242b89fb1e306
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-04 11:56:00 +0600

    simple snake,water,gun game created using javascript

---
+++ loops/simpleGame.js
@@ -0,0 +1,2 @@
+//This is a simple game of snake,water and gun. Here user has to enter g,s or w and computer will enter a value of his own. 
+//User has to enter the number of time he wants to play.
---
+++ loops/whileLoopTest.js
@@ -0,0 +1,23 @@
+//this code won't run on vs code because prompt doesn't work here
+let runAgain = true;
+
+const canDrive = (age) => {
+  return age >= 18 ? true : false
+}
+
+while (runAgain) {
+  let age = prompt("Enter your age")
+  age = Number.parseInt(age)
+  if (age < 0) {
+    console.error("Please enter a valid age");
+    break;
+  }
+
+  if (canDrive(age)) {
+    alert("Yes you can drive")
+  }
+  else {
+    alert("You cannot drive")
+  }
+  runAgain = confirm("Do you want to play again?")
+}
