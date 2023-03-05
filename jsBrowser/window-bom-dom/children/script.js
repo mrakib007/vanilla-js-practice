2d58ffa939576a3a2f5c54e54d476905f1c1e326
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-05 23:52:27 +0600

    Practicing parent and children nodes

--- jsBrowser/window-bom-dom/children/script.js
+++ jsBrowser/window-bom-dom/children/script.js
@@ -3,3 +3,11 @@ console.log(document.body.firstChild);
 console.log(document.body.lastChild);
 let arr = Array.from(document.body.childNodes);
 console.log(arr);
+
+console.log(document.body.children);
+const arr2 = document.body.children;
+for (const iterator of arr2) {
+    console.log(iterator);
+}
+console.log(arr2[1].children[0].innerText);
+console.log(arr2[1].children[1].innerText);
