141320bcd07cb054145d7e988aec7e8c7b677e51
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-08 13:02:43 +0600

    more methods

--- jsBrowser/window-bom-dom/html-insertion-method/script.js
+++ jsBrowser/window-bom-dom/html-insertion-method/script.js
@@ -6,4 +6,11 @@ div.innerHTML = '<h1>Hello Prepend</h1>';
 // a.append(div);
 a.prepend(div);
 
-// a.replaceWith(div);
+a.replaceWith(div);
+
+second.insertAdjacentHTML('beforebegin','<div class="test">Before begin</div>');
+second.insertAdjacentHTML('beforeend','<div class="test">Before end</div>');
+second.insertAdjacentHTML('afterbegin','<div class="test">After Begin</div>');
+second.insertAdjacentHTML('beforeend','<div class="test">After End</div>');
+
+// second.remove();
