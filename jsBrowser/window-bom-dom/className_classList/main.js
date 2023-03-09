b6ea95db46db4f82708b31b128233548593036b1
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-09 13:54:11 +0600

    playing with classList,toggle,add,remove

---
+++ jsBrowser/window-bom-dom/className_classList/main.js
@@ -0,0 +1,6 @@
+first.className = "text-dark red";
+first.classList.remove("red");
+first.classList.add('red');
+first.classList.toggle('text-dark');
+console.log(first.classList.contains("red"));
+console.log(first.classList);
