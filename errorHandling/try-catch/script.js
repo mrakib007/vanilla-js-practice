2f29240205c4a9ffcf30a0f043acf1697a0c830a
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-18 23:14:23 +0600

    try and catch practicing

---
+++ errorHandling/try-catch/script.js
@@ -0,0 +1,28 @@
+setTimeout(()=>{
+    console.log('Hacking wifi...please wait...');
+},1000);
+
+try{
+    // console.log(sakib);
+    setTimeout(()=>{
+        // console.log(sakib);
+        try {
+            console.log(sakib);
+        } catch (error) {
+            console.log(error, "this is error")
+        }
+    },100)
+}catch(error){
+    console.log('This is error' + error);
+}
+
+setTimeout(()=>{
+    console.log('Fetching username and password');
+},2000);
+setTimeout(()=>{
+    console.log('Hacking Sakibs id');
+},3000);
+setTimeout(()=>{
+    console.log('Fetching username of Sakib and password');
+},4000);
+
