4a60d181487d6df2b250985da9992b2eba3d540a
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-03-28 20:40:43 +0600

    simple todo list app using localstorage

--- jsBrowser/LocalStorage/todoList/script.js
+++ jsBrowser/LocalStorage/todoList/script.js
@@ -1,29 +1,53 @@
-const todoInput = document.getElementById('todo-input');
-const addBtn = document.getElementById('add-button');
-const todoTable = document.getElementById('todo-table');
+// Get the saved tasks from local storage
+let savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+// Add an event listener to the Add button
+const addButton = document.getElementById('add-button');
+addButton.addEventListener('click', function () {
+  // Get the input value and create a new task object
+  const inputField = document.getElementById('todo-input');
+  const taskText = inputField.value;
+  const newTask = {
+    text: taskText,
+    done: false,
+  };
 
-// savedTasks.map((task)=>{
-//     addTaskToDom(task);
-// })
+  // Add the new task to the savedTasks array and save it to local storage
+  savedTasks.push(newTask);
+  localStorage.setItem('tasks', JSON.stringify(savedTasks));
 
-addBtn.addEventListener('click',function(){
-    const taskText = todoInput.value;
-    if(taskText !== ''){
-    const task = {
-        text : taskText,
-        id : Date.now(),
-    }
-    savedTasks.push(task);
+  // Add the new task to the table
+  addTaskToTable(newTask);
+
+  // Clear the input field
+  inputField.value = '';
+});
+
+// Function to add a task to the table
+function addTaskToTable(task) {
+  const tableBody = document.getElementById('todo-table-body');
+  const newRow = tableBody.insertRow();
+
+  const taskCell = newRow.insertCell();
+  taskCell.textContent = task.text;
+
+  const deleteCell = newRow.insertCell();
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.className = 'btn btn-danger';
+  deleteButton.addEventListener('click', function () {
+    // Remove the task from the savedTasks array and save it to local storage
+    const index = savedTasks.indexOf(task);
+    savedTasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(savedTasks));
+
+    // Remove the task from the table
+    tableBody.removeChild(newRow);
+  });
+  deleteCell.appendChild(deleteButton);
 }
 
-    localStorage.setItem('tasks',JSON.stringify(savedTasks));
-    // addTaskToDom(task);
-    todoInput.value = '';
-})
-
-// const addTaskToDom = (task) =>{
-//     console.log(task)
-// }
-
+// Add the saved tasks to the table
+for (let i = 0; i < savedTasks.length; i++) {
+  addTaskToTable(savedTasks[i]);
+}
